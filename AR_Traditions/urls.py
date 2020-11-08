c8a9a567a77b2eaa0ee4e2dbe96d7f7a54bd7b16
Omar Najera <omar.navzz@gmail.com>
2020-11-08 13:39:39 -0600

    Urls

--- AR_Traditions/urls.py
+++ AR_Traditions/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.config import settings
+from django.conf import settings
 from django.config.urls.static import static
 
 from AR_Camera import views
